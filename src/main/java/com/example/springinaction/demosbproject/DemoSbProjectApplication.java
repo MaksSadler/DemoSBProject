3bc728b060723a571f3124d3388c0aa208763446
Max <max0sadler@gmail.com>
2025-11-05 18:51:25 +0300

    Create new spring boot project

---
+++ src/main/java/com/example/springinaction/demosbproject/DemoSbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinaction.demosbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springinaction/demosbproject/DemoSbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinaction.demosbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
