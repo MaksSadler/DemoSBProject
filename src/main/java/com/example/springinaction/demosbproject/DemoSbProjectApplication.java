5957d4a22391fa68b831d8c8b23d0e9b86e0a3d0
Max <max0sadler@gmail.com>
2025-11-10 19:49:08 +0300

    Create and add new classes for spring boot project

--- src/main/java/com/example/springinaction/demosbproject/DemoSbProjectApplication.java
+++ src/main/java/com/example/springinaction/demosbproject/DemoSbProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.springinaction.demosbproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoSbProjectApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/example/springinaction/demosbproject/DemoSbProjectApplicationTests.java
+++ src/test/java/com/example/springinaction/demosbproject/DemoSbProjectApplicationTests.java
@@ -1,13 +1,88 @@
 package com.example.springinaction.demosbproject;
 
+import com.example.springinaction.demosbproject.repository.ExchangeRatesRepository;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
-class DemoSbProjectApplicationTests {
+@Transactional
+@AutoConfigureMockMvc
+@AutoConfigureWireMock(port = 0)
+@ActiveProfiles("integration")
+public class DemoSbProjectApplicationTests {
+    @Autowired
+    private WireMockServer wireMockServer;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ExchangeRatesRepository repository;
 
-    @Test
-    void contextLoads() {
+    @BeforeAll
+    public static void setup() {
+        stubFor(WireMock.get(urlMatching("/xml/30/01/2025"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
+                        .withBodyFile("xml/positive_response.xml")));
+
+        stubFor(WireMock.get(urlMatching("/xml/31/01/2025"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.BAD_REQUEST.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)));
     }
 
+    @Test
+    public void happyPathXmlTest() throws Exception {
+        mockMvc.perform(get("/exchanger")
+                        .param("currency", "AUD")
+                        .param("date", "2025-01-30")
+                        .param("dataSource", "xml"))
+                .andExpect(content().string("47.6537"));
+    }
+
+    @Test
+    @Sql("/files/database/data.sql")
+    public void happyPathDatabaseTest() throws Exception {
+        mockMvc.perform(get("/exchanger")
+                        .param("currency", "AUD")
+                        .param("date", "2025-01-10")
+                        .param("dataSource", "database"))
+                .andExpect(content().string("47.6537"));
+    }
+
+    @Test
+    @Sql("/files/database/data.sql")
+    public void noCurrencyFoundInDatabaseTest() throws Exception {
+        mockMvc.perform(get("/exchanger")
+                        .param("currency", "XXX")
+                        .param("date", "2025-01-30")
+                        .param("dataSource", "database"))
+                .andExpect(content().string("No such currency found."));
+    }
+
+    @Test
+    public void noXmlDtoTest() throws Exception {
+        mockMvc.perform(get("/exchanger")
+                        .param("currency", "AUD")
+                        .param("date", "2025-01-31")
+                        .param("dataSource", "xml"))
+                .andExpect(content().string("Unable to get exchange rate data from the source."));
+    }
 }
